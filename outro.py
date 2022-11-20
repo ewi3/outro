dc7a3491417616192ef567bffa0257f9c5f0377b
ewi3 <111629875+ewi3@users.noreply.github.com>
2022-11-19 20:19:53 -0800

    Add files via upload
    if you have version 107 of chrome use this version of chromedriver, there might be any errors in the program but it can be used.

--- outro.py
+++ outro.py
@@ -3,25 +3,45 @@ from selenium.webdriver.common.by import By
 import time
 import os
 
-
-timer = 18
 cmd = 'shutdown /p '
-
-driver = wd.Chrome(executable_path= '..\Desktop\chromedriver.exe')
-
-driver.get('https://www.youtube.com/watch?v=FX9eEhoRZhY')
-
-play_button = driver.find_element(By.XPATH, '//*[@id="movie_player"]/div[27]/div[2]/div[1]/button')
-play_button.click()
-wd.Chrome.minimize_window(driver)
+usr_input = input("Do you want to add a set time for the timer? Y/N")
+if usr_input == 'Y':
+    timer = int(input("Then, how much time do you want? (seconds)"))
 
 
-for i in range(timer):
-    time.sleep(1)
-    timer = timer - 1
-    print("{} seconds left ".format(timer))
+def init_of_webdriver():
+    path = wd.Chrome(executable_path='.\chromedriver.exe')
+    path.get('https://www.youtube.com/watch?v=FX9eEhoRZhY')
+    play_button = path.find_element(By.XPATH, '//*[@id="movie_player"]/div[28]/div[2]/div[1]/button')
+    play_button.click()
+    wd.Chrome.minimize_window(path)
+    # this function starts the webDriver
 
-if timer == 0:
-    print("Byeee!!")
-    time.sleep(1)
-    os.system(cmd)
+
+def default_time(path):
+    if usr_input == "N":
+        duration = path.find_elements(By.XPATH, "//span[@class='ytp-time-duration']")[0]
+        print('Video Length = ' + duration.text)
+        duration_replaced = duration.text.replace(":", "")
+        timer = (int(duration_replaced) / 600) * 4 + 12
+    # this function sets a default time by getting the duration of the video
+
+
+def main(timer):
+    init_of_webdriver()
+
+    for i in range(int(timer)):
+        time.sleep(1)
+        timer = timer - 1
+        print(f'{timer} seconds left ')
+    # this part prints the time by getting the timer of default_time()
+
+    if timer >= 0:
+        print("Byeee!!")
+        time.sleep(1)
+        os.system(cmd)
+    # this part shuts down the computer when the timer is 0 or almost 0
+
+
+if __name__ == '__main__':
+    main(timer)
